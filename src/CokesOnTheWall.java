f97cfbec57510c77fdeece80719859498dfe103f
MaherFayezk <92121234+MaherFayezk@users.noreply.github.com>
2021-10-20 10:48:42 +0200

    Initial commit

---
+++ src/CokesOnTheWall.java
@@ -0,0 +1,28 @@
+public class CokesOnTheWall {
+    public static void main(String[] args) {
+
+        /*
+        Write a program that outputs the lyrics for "Ninety-nine Bottles of Coke on the Wall.
+        "  Your program should print the number of the bottles in English, not as a number.  For example:
+        Ninety-nine bottles of coke on the wall. ... One bottle of coke on the wall. One bottle of coke, Take one down, pass it around, Zero bottles of coke on the wall.
+        Your program must not just have 99 output statements. You need to use loops, do not write 99 print statements
+
+        your output should look like the following
+        Ninety-Nine bottles of coke on the wall. Ninety-Nine bottles of coke. Take one down and pass it around
+        Ninety-Eight bottles of coke on the wall. Ninety-Eight bottles of coke. Take one down and pass it around
+        Ninety-Seven bottles of coke on the wall. Ninety-Seven bottles of coke. Take one down and pass it around
+        Ninety-Six bottles of coke on the wall. Ninety-Six bottles of coke. Take one down and pass it around
+        Ninety-Five bottles of coke on the wall. Ninety-Five bottles of coke. Take one down and pass it around
+        Ninety-Four bottles of coke on the wall. Ninety-Four bottles of coke. Take one down and pass it around
+        Ninety-Three bottles of coke on the wall. Ninety-Three bottles of coke. Take one down and pass it around
+        .................
+        Five bottles of coke on the wall. Five bottles of coke. Take one down and pass it around
+        Four bottles of coke on the wall. Four bottles of coke. Take one down and pass it around
+        Three bottles of coke on the wall. Three bottles of coke. Take one down and pass it around
+        Two bottles of coke on the wall. Two bottles of coke. Take one down and pass it around
+        One bottles of coke on the wall. One bottles of coke. Take one down and pass it around,Zero bottles of coke on the wall.
+         */
+        //Remove these comments and enjoy solving the exercise
+
+    }
+}
---
+++ src/KnockOut.java
@@ -0,0 +1,53 @@
+public class KnockOut {
+    public static void main(String[] args) {
+
+        //1.The player chooses their 'knock out' number – either 6, 7, 8, or 9. Take this choice as input.
+        //2.The computer's 'knock out' number is randomly chosen by your program – either 6, 7, 8, or 9.
+        //3.Write a loop that rolls both dice for the player and keeps track of the total sum of dice rolls.
+        //4.Add only the sums that do not equal the knockout
+        // 5.Repeat until the 'knock out' number is rolled. Do not add the 'knock out' number to the sum.
+        //6.Once the player's turn is over, repeat step 3 for the computer's turn.
+        //7.Continue until either the player or the computer reaches 100. The first to do so wins.
+        // your output should look like the following
+        /*Enter a knockout number between 6 and 9
+        7
+        your knockout is 7  comp knock out 8
+         your rolling result   5
+        your current score is 5
+         your rolling result   4
+        your current score is 9
+         your rolling result   5
+        your current score is 14
+         your rolling result   7
+        knocked out... current score is   :  14
+        computer rolling result is  6
+        computer rolling result is  6
+        computer rolling result is  5
+        computer rolling result is  6
+        computer rolling result is  6
+        computer rolling result is  5
+        computer rolling result is  9
+        computer rolling result is  9
+        computer rolling result is  10
+        computer rolling result is  9
+        computer rolling result is  6
+        computer rolling result is  3
+        computer rolling result is  7
+        computer rolling result is  6
+        computer rolling result is  4
+        computer rolling result is  8
+        Machine Knocked out..... Machine Current Score is :  97
+         your rolling result   4
+        your current score is 18
+         your rolling result   3
+        your current score is 21
+         your rolling result   8
+        your current score is 29
+         your rolling result   7
+        knocked out... current score is   :  29
+        computer rolling result is  4
+        computer rolling result is  9
+        Comp win and the comp score is  101 */
+        // Remove these comments and have fun solving this exercise
+    }
+}
