1a2607bd277ab7b6b58bf89b490bd73f78b0d38b
hannaelrakaybi <Hanna.Elrakaybi@uofcanada.edu.eg>
2021-10-25 02:16:50 +0200

    done

--- src/CokesOnTheWall.java
+++ src/CokesOnTheWall.java
@@ -1,28 +1,145 @@
+import java.util.Scanner;
+
 public class CokesOnTheWall {
+    private int numberOfBottles;
+
+    CokesOnTheWall(int number) {
+        if (number < 0) {
+            this.numberOfBottles = 0;
+        } else if (number > 99) {
+            this.numberOfBottles = 99;
+        } else {
+            this.numberOfBottles = number;
+        }
+
+    }
+
+    public String getSingular(int i) {
+        switch(i) {
+            case 0:
+                return "Zero";
+            case 1:
+                return "One";
+            case 2:
+                return "Two";
+            case 3:
+                return "Three";
+            case 4:
+                return "Four";
+            case 5:
+                return "Five";
+            case 6:
+                return "Six";
+            case 7:
+                return "Seven";
+            case 8:
+                return "Eight";
+            case 9:
+                return "Nine";
+            default:
+                return "Error";
+        }
+    }
+
+    public String getMultiplesOfTen(int i) {
+        switch(i) {
+            case 2:
+                return "Twenty";
+            case 3:
+                return "Thirty";
+            case 4:
+                return "Forty";
+            case 5:
+                return "Fifty";
+            case 6:
+                return "Sixty";
+            case 7:
+                return "Seventy";
+            case 8:
+                return "Eighty";
+            case 9:
+                return "Ninety";
+            default:
+                return "Error";
+        }
+    }
+
+    public String getTeens(int i) {
+        switch(i) {
+            case 0:
+                return "Ten";
+            case 1:
+                return "Eleven";
+            case 2:
+                return "Twelve";
+            case 3:
+                return "Thirteen";
+            case 4:
+                return "Fourteen";
+            case 5:
+                return "Fifteen";
+            case 6:
+                return "Sixteen";
+            case 7:
+                return "Seventeen";
+            case 8:
+                return "Eighteen";
+            case 9:
+                return "Nineteen";
+            default:
+                return "Error";
+        }
+    }
+
+    public String getNumberInEnglish(int i) {
+        int ones = i % 10;
+        int tens = i / 10;
+        if (i > 9) {
+            if (tens == 1) {
+                return this.getTeens(ones);
+            } else if (ones == 0) {
+                return this.getMultiplesOfTen(i / 10);
+            } else {
+                String var10000 = this.getMultiplesOfTen(tens);
+                return var10000 + "-" + this.getSingular(ones);
+            }
+        } else {
+            return this.getSingular(ones);
+        }
+    }
+
+    public String printSong() {
+        String bottleString = "bottles";
+        String Output = "";
+
+        while(this.numberOfBottles > 0) {
+            if (this.numberOfBottles == 1) {
+                bottleString = "bottle";
+            }
+
+            Output = Output + this.getNumberInEnglish(this.numberOfBottles) + " " + bottleString + " of beer on the wall.\n";
+            Output = Output + this.getNumberInEnglish(this.numberOfBottles) + " " + bottleString + " of beer on the wall,\n";
+            Output = Output + "Take one down, pass it around,\n";
+            --this.numberOfBottles;
+            if (this.numberOfBottles == 0) {
+                bottleString = "bottles";
+            } else if (this.numberOfBottles == 1) {
+                bottleString = "bottle";
+            }
+        }
+
+        Output = Output + "Zero bottles of beer on the wall.";
+        return Output;
+    }
+}
+
+class Sing {
     public static void main(String[] args) {
-
-        /*
-        Write a program that outputs the lyrics for "Ninety-nine Bottles of Coke on the Wall.
-        "  Your program should print the number of the bottles in English, not as a number.  For example:
-        Ninety-nine bottles of coke on the wall. ... One bottle of coke on the wall. One bottle of coke, Take one down, pass it around, Zero bottles of coke on the wall.
-        Your program must not just have 99 output statements. You need to use loops, do not write 99 print statements
-
-        your output should look like the following
-        Ninety-Nine bottles of coke on the wall. Ninety-Nine bottles of coke. Take one down and pass it around
-        Ninety-Eight bottles of coke on the wall. Ninety-Eight bottles of coke. Take one down and pass it around
-        Ninety-Seven bottles of coke on the wall. Ninety-Seven bottles of coke. Take one down and pass it around
-        Ninety-Six bottles of coke on the wall. Ninety-Six bottles of coke. Take one down and pass it around
-        Ninety-Five bottles of coke on the wall. Ninety-Five bottles of coke. Take one down and pass it around
-        Ninety-Four bottles of coke on the wall. Ninety-Four bottles of coke. Take one down and pass it around
-        Ninety-Three bottles of coke on the wall. Ninety-Three bottles of coke. Take one down and pass it around
-        .................
-        Five bottles of coke on the wall. Five bottles of coke. Take one down and pass it around
-        Four bottles of coke on the wall. Four bottles of coke. Take one down and pass it around
-        Three bottles of coke on the wall. Three bottles of coke. Take one down and pass it around
-        Two bottles of coke on the wall. Two bottles of coke. Take one down and pass it around
-        One bottles of coke on the wall. One bottles of coke. Take one down and pass it around,Zero bottles of coke on the wall.
-         */
-        //Remove these comments and enjoy solving the exercise
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a number:");
+        int numberOfBottles = sc.nextInt();
+        CokesOnTheWall song = new CokesOnTheWall(numberOfBottles);
+        System.out.print(song.printSong());
+        sc.close();
     }
 }
--- src/KnockOut.java
+++ src/KnockOut.java
@@ -1,53 +1,30 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class KnockOut {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your Knock out number (6, 7, 8, 9):");
+        int playerKO = input.nextInt();
+        Random random = new Random();
+        int ComputerKo = random.nextInt(4) + 6;
+        System.out.println("The computer's knock out number is " + ComputerKo);
+        int playerSum = 0;
+        int computerSum = 0;
+        int roll = 0;
 
-        //1.The player chooses their 'knock out' number – either 6, 7, 8, or 9. Take this choice as input.
-        //2.The computer's 'knock out' number is randomly chosen by your program – either 6, 7, 8, or 9.
-        //3.Write a loop that rolls both dice for the player and keeps track of the total sum of dice rolls.
-        //4.Add only the sums that do not equal the knockout
-        // 5.Repeat until the 'knock out' number is rolled. Do not add the 'knock out' number to the sum.
-        //6.Once the player's turn is over, repeat step 3 for the computer's turn.
-        //7.Continue until either the player or the computer reaches 100. The first to do so wins.
-        // your output should look like the following
-        /*Enter a knockout number between 6 and 9
-        7
-        your knockout is 7  comp knock out 8
-         your rolling result   5
-        your current score is 5
-         your rolling result   4
-        your current score is 9
-         your rolling result   5
-        your current score is 14
-         your rolling result   7
-        knocked out... current score is   :  14
-        computer rolling result is  6
-        computer rolling result is  6
-        computer rolling result is  5
-        computer rolling result is  6
-        computer rolling result is  6
-        computer rolling result is  5
-        computer rolling result is  9
-        computer rolling result is  9
-        computer rolling result is  10
-        computer rolling result is  9
-        computer rolling result is  6
-        computer rolling result is  3
-        computer rolling result is  7
-        computer rolling result is  6
-        computer rolling result is  4
-        computer rolling result is  8
-        Machine Knocked out..... Machine Current Score is :  97
-         your rolling result   4
-        your current score is 18
-         your rolling result   3
-        your current score is 21
-         your rolling result   8
-        your current score is 29
-         your rolling result   7
-        knocked out... current score is   :  29
-        computer rolling result is  4
-        computer rolling result is  9
-        Comp win and the comp score is  101 */
-        // Remove these comments and have fun solving this exercise
+        while(roll != playerKO && playerSum < 100) {
+            roll = random.nextInt(12) + 1;
+            System.out.printf("Player roll : %d%n", roll);
+            playerSum += roll;
+            System.out.printf("Player total : %d%n", playerSum);
+        }
+
+        while(roll != ComputerKo && computerSum < 100) {
+            roll = random.nextInt(12) + 1;
+            System.out.printf("Computer roll : %d%n", roll);
+            computerSum += roll;
+            System.out.printf("Computer total : %d%n", computerSum);
+        }
     }
 }
